de998430fd85b7ca2e32264e399c9cecf02dcdec
saranyachandra <saranya.arapps@gmail.com>
2016-10-14 18:14:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ function moveRight(){
     
     marginLeft=marginLeft +10;
     img.marginLeft=marginLeft + "px";
-}
+};
 img.onclick=function(){
     var interval=setInterval(moveRight,100);
    
