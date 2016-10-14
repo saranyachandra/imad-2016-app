3dfd30020dd5d706885b79712934568fa59c6873
saranyachandra <saranya.arapps@gmail.com>
2016-10-14 18:08:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,13 @@ element.innerHTML="new value";
 
 //move the image
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    
+    marginLeft=marginLeft +10;
+    img.marginLeft=marginLeft + "px";
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+   
     };
