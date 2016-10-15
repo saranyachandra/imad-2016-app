cc15cbdf48e641ff61435511b972ccb73e226048
saranyachandra <saranya.arapps@gmail.com>
2016-10-15 11:43:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ app.get('/', function (req, res) {
 });
 var counter=0;
 app.get('/counter',function(req, res){
-    counter = counter+1;
-    res.send(counter.tostring());
+    counter = counter + 1;
+    res.send(counter.toString());
 });
 app.get('/:articleName',function(req,res){
     //articleName==articleone
