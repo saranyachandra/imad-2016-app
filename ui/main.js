3bada62e038369abe7dc4ac87fea021d0694018b
saranyachandra <saranya.arapps@gmail.com>
2016-10-15 11:55:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,7 @@
-console.log('Loaded!');
-
-
-//change the text for main div
-var element=document.getElementById('main-text');
-element.innerHTML='new value';
-
-//move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-    }
-img.onclick = function(){
-   var interval = setInterval(moveRight, 25);
-};
-    
-   
+var button = document.getElementById('counter');
+var counter = 0;
+ button.onclick = function(){
+     counter = counter + 1;
+     var span = document.getElementById('count');
+     span.innerHTML = counter.toString();
+ };
