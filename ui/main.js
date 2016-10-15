6a214efe51d6711eaffe84dd8e3877267d58a744
saranyachandra <saranya.arapps@gmail.com>
2016-10-15 11:04:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ element.innerHTML='new value';
 //move the image
 var img = document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginleft = '100px';
+    img.style.marginLeft = '100px';
 };
     
    
